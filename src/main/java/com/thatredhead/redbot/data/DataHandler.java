467d5d18c45b7d5a6438b84dc91eb2cd272d3eee
James Gallicchio <jamesgallicchio@gmail.com>
2017-01-17 09:47:50 -0500

    Fix FNF exception being where it shouldn't
    - Removes thrown exception from DataHandler

--- src/main/java/com/thatredhead/redbot/data/DataHandler.java
+++ src/main/java/com/thatredhead/redbot/data/DataHandler.java
@@ -29,7 +29,7 @@ public class DataHandler {
         }
     }
 
-    public PermissionHandler getPermHandler() throws FileNotFoundException {
+    public PermissionHandler getPermHandler() {
         return permh;
     }
 
