346dea665a354d5eff7bf4fe2cdf36e749259ec3
James Gallicchio <jamesgallicchio@gmail.com>
2017-02-03 07:27:07 -0500

    Add MathCommands (not functional)

---
+++ src/main/java/com/thatredhead/redbot/command/impl/MathCommands.java
@@ -0,0 +1,7 @@
+package com.thatredhead.redbot.command.impl;
+
+import com.thatredhead.redbot.command.ICommandGroup;
+import com.thatredhead.jmath.*;
+
+public class MathCommands extends ICommandGroup {
+}
