405ded9a5c9c140009b276924c5263c9d8599718
James Gallicchio <jamesgallicchio@gmail.com>
2017-03-02 11:04:53 -0500

    Document CommandHandler & DataHandler
    All the javadocs

--- src/main/java/com/thatredhead/redbot/data/DataHandler.java
+++ src/main/java/com/thatredhead/redbot/data/DataHandler.java
@@ -63,24 +63,48 @@ public class DataHandler {
     }
 
     /**
-     *
-     * @param o
-     * @param name
-     * @return
+     * Saves an object in json form
+     * @param obj the object to save
+     * @param name the name to save it under
+     * @return o, for chaining methods
      */
     public <T> T save(T obj, String name) {
         writeToFile(new File("data/" + name + ".json"), gson.toJson(obj));
         return obj;
     }
 
+    /**
+     * Gets an object from json
+     * @param name the name the object is saved under
+     * @param classOfT a class instance of T
+     * @param <T> the type of the object to get
+     * @return the object gotten from json
+     * @throws FileNotFoundException
+     */
     public <T> T get(String name, Class<T> classOfT) throws FileNotFoundException {
         return gson.fromJson(readFromFile("data/" + name + ".json"), classOfT);
     }
 
+    /**
+     * Gets an object from json
+     * @param name the name the object is saved under
+     * @param T the type of the object to get
+     * @param <T> the type of the object to get
+     * @return the object gotten from json
+     * @throws FileNotFoundException
+     */
     public <T> T get(String name, Type T) throws FileNotFoundException {
         return gson.fromJson(readFromFile("data/" + name + ".json"), T);
     }
 
+    /**
+     * Gets an object or defaults to another object if it doesn't exist
+     * @param name the name of the object to get
+     * @param classOfT a class instance of T
+     * @param def the default object to use if name isn't present
+     * @param <T> the type of object to get
+     * @return the object gotten from json, or default
+     */
     public <T> T get(String name, Class<T> classOfT, T def) {
         try {
             T obj = get(name, classOfT);
@@ -91,6 +115,14 @@ public class DataHandler {
         }
     }
 
+    /**
+     * Gets an object or defaults to another object if it doesn't exist
+     * @param name the name of the object to get
+     * @param T the Type of the object to get
+     * @param def the default object to use if name isn't present
+     * @param <T> the type of object to get
+     * @return the object gotten from json, or default
+     */
     public <T> T get(String name, Type T, T def) {
         try {
             T obj = get(name, T);
@@ -100,7 +132,7 @@ public class DataHandler {
             return save(def, name);
         }
     }
-
+    
     private static String readFromFile(String s) throws FileNotFoundException {
         return readFromFile(Paths.get(s));
     }
