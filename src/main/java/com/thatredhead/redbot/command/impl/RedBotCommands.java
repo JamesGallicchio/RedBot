b5fa2ada0463adbafa53914222ae244d2de41834
ThatRedhead <jamesgallicchio@gmail.com>
2017-03-27 21:10:38 -0400

    Refactor system commands
    - Commands for me go in SystemCommands
    - Commands for everyone go in RedBotCommands

---
+++ src/main/java/com/thatredhead/redbot/command/impl/RedBotCommands.java
@@ -0,0 +1,30 @@
+package com.thatredhead.redbot.command.impl;
+
+import com.thatredhead.redbot.RedBot;
+import com.thatredhead.redbot.command.Command;
+import com.thatredhead.redbot.command.CommandException;
+import com.thatredhead.redbot.command.CommandGroup;
+import com.thatredhead.redbot.helpers4d4j.DiscordUtils;
+import com.thatredhead.redbot.helpers4d4j.MessageParser;
+import com.thatredhead.redbot.permission.PermissionContext;
+
+import java.util.Arrays;
+
+public class RedBotCommands extends CommandGroup {
+
+    public RedBotCommands() {
+        super("RedBot Commands", "General commands for users and administrators", "redbot", Arrays.asList(new InviteCommand()));
+    }
+
+    public static class InviteCommand extends Command {
+
+        public InviteCommand() {
+            super("invite", "Gives a link to invite the bot to other servers", PermissionContext.EVERYONE);
+        }
+
+        @Override
+        public void invoke(MessageParser msgp) throws CommandException {
+            DiscordUtils.sendMessage("Invite me to another guild: " + RedBot.INVITE, msgp.getChannel());
+        }
+    }
+}
--- src/main/java/com/thatredhead/redbot/command/impl/SystemCommands.java
+++ src/main/java/com/thatredhead/redbot/command/impl/SystemCommands.java
@@ -1,10 +1,10 @@
 package com.thatredhead.redbot.command.impl;
 
-import com.thatredhead.redbot.helpers4d4j.DiscordUtils;
 import com.thatredhead.redbot.RedBot;
 import com.thatredhead.redbot.command.Command;
 import com.thatredhead.redbot.command.CommandException;
 import com.thatredhead.redbot.command.CommandGroup;
+import com.thatredhead.redbot.helpers4d4j.DiscordUtils;
 import com.thatredhead.redbot.helpers4d4j.MessageParser;
 import com.thatredhead.redbot.permission.PermissionContext;
 
@@ -13,7 +13,7 @@ import java.util.Arrays;
 public class SystemCommands extends CommandGroup {
 
     public SystemCommands() {
-        super("System Commands", "Commands pertinent to RedBot's system", "system", Arrays.asList(new InfoCommand(), new InviteCommand()));
+        super("System Commands", "Commands for administrative purposes (for ThatRedhead)", "system", Arrays.asList(new InfoCommand()));
     }
 
     public static class InfoCommand extends Command {
@@ -25,6 +25,7 @@ public class SystemCommands extends CommandGroup {
         public void invoke(MessageParser msgp) throws CommandException {
             StringBuilder info = new StringBuilder("**System Info**");
             info.append("\nGuild count: ").append(RedBot.getClient().getGuilds().size());
+            info.append("\nUser count: ").append(RedBot.getClient().getUsers().size());
             info.append("\nCurrent uptime: ").append(RedBot.getUptime());
             info.append("\nThread count: ").append(Thread.activeCount());
             long total = Runtime.getRuntime().totalMemory();
@@ -32,18 +33,6 @@ public class SystemCommands extends CommandGroup {
             info.append("\nMemory usage (MB): ").append((total-free)/1024/1024).append("/").append(total/1024/1024);
             info.append("\nVersion: ").append(RedBot.getVersion());
             DiscordUtils.sendMessage(info.toString(), msgp.getChannel());
-        }
-    }
-
-    public static class InviteCommand extends Command {
-
-        public InviteCommand() {
-            super("invite", "Gives a link to invite the bot to other servers", PermissionContext.EVERYONE);
-        }
-
-        @Override
-        public void invoke(MessageParser msgp) throws CommandException {
-            DiscordUtils.sendMessage("Invite me to another guild: " + RedBot.INVITE, msgp.getChannel());
         }
     }
 }
