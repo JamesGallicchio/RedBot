15cb3bdbf0181c31125e95f809b604381a8eb754
ThatRedhead <jamesgallicchio@gmail.com>
2017-05-04 18:41:51 -0400

    Modify Test command for testing messagehistory

--- src/main/java/com/thatredhead/redbot/command/impl/TestCommand.java
+++ src/main/java/com/thatredhead/redbot/command/impl/TestCommand.java
@@ -4,6 +4,10 @@ import com.thatredhead.redbot.command.Command;
 import com.thatredhead.redbot.command.CommandException;
 import com.thatredhead.redbot.helpers4d4j.MessageParser;
 import com.thatredhead.redbot.permission.PermissionContext;
+import sx.blah.discord.util.MessageHistoryBuilder;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class TestCommand extends Command {
 
@@ -13,6 +17,15 @@ public class TestCommand extends Command {
 
     @Override
     public void invoke(MessageParser msgp) throws CommandException {
-        msgp.reply("Pong!", "Testy test test tester testing test, testing tested tester tests", false);
+
+        String[] history = new MessageHistoryBuilder(msgp.getChannel())
+                .withMaxCount(5)
+                .build()
+                .stream()
+                .flatMap(msg -> Arrays.stream(new String[]{msg.getStringID(), msg.getContent()}))
+                .collect(Collectors.toList())
+                .toArray(new String[10]);
+
+        msgp.reply("Pong!", "Testy test test tester testing test, testing tested tester tests", false, history);
     }
 }
