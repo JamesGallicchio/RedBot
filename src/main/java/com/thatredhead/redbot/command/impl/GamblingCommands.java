ed07d5279d75ed18bebdab7e276a1fd10637ad84
James Gallicchio <jamesgallicchio@gmail.com>
2017-05-16 09:32:31 -0400

    Add magic 8 ball

--- src/main/java/com/thatredhead/redbot/command/impl/GamblingCommands.java
+++ src/main/java/com/thatredhead/redbot/command/impl/GamblingCommands.java
@@ -19,8 +19,33 @@ public class GamblingCommands  extends CommandGroup {
     public GamblingCommands() {
         super("GamblingCommands", "Commands for playing a variety of gambling games",
                 "gambling", Arrays.asList(
-                    new SlotsCommand()
+                        new SlotsCommand(),
+                        new Magic8Ball()
                 ));
+    }
+
+    public static class Magic8Ball extends Command {
+
+        public static String[] responses = {
+                "It is certain", "It is decidedly so",
+                "Without a doubt", "Yes definitely",
+                "You may rely on it", "As I see it, yes",
+                "Most likely", "Outlook good",
+                "Yes", "Signs point to yes",
+                "Reply hazy try again", "Ask again later",
+                "Better not tell you now", "Cannot predict now",
+                "Concentrate and ask again", "Don't count on it",
+                "My reply is no", "My sources say no",
+                "Outlook not so good", "Very doubtful"
+        };
+
+        public Magic8Ball() {
+            super("magic8ball", "Shakes a magic 8 ball", PermissionContext.EVERYONE);
+        }
+
+        public void invoke(MessageParser msgp) {
+            msgp.reply(Utilities4D4J.makeEmbed("Magic 8 Ball says...", responses[(int) (responses.length*Math.random())], true));
+        }
     }
 
     public static class SlotsCommand extends Command {
