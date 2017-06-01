d8dcd61dc0b173f430b2d585820f940014f5fdf2
James Gallicchio <jamesgallicchio@gmail.com>
2017-05-31 20:56:29 -0400

    Couple of economy fixes

--- src/main/java/com/thatredhead/redbot/econ/Economy.java
+++ src/main/java/com/thatredhead/redbot/econ/Economy.java
@@ -62,10 +62,14 @@ public class Economy {
 
         for(int i = 1; i < accounts.size(); i++) {
             int j = i-1;
+            if (accounts.get(i).getOwner() == null) {
+                accounts.remove(i--);
+                continue;
+            }
             double amount = accounts.get(i).getAmount();
             while(j >= 0 && accounts.get(j).getAmount() < amount)
                 j--;
-            accounts.add(j+1, accounts.remove(i));
+            accounts.set(i, accounts.set(j+1, accounts.get(i)));
         }
     }
 }
