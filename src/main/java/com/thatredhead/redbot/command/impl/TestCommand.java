be1434f51e1c6ce38c80c056515bd9d44d262da8
ThatRedhead <jamesgallicchio@gmail.com>
2017-06-14 23:40:22 -0400

    Revert back to my repo copy for D4J (now up to date)

--- src/main/java/com/thatredhead/redbot/command/impl/TestCommand.java
+++ src/main/java/com/thatredhead/redbot/command/impl/TestCommand.java
@@ -4,8 +4,6 @@ import com.thatredhead.redbot.command.Command;
 import com.thatredhead.redbot.command.CommandException;
 import com.thatredhead.redbot.helpers4d4j.MessageParser;
 import com.thatredhead.redbot.permission.PermissionContext;
-import com.vdurmont.emoji.Emoji;
-import com.vdurmont.emoji.EmojiManager;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,9 +13,6 @@ public class TestCommand extends Command {
     public TestCommand() {
         super("test", "TESTY COMMANDS", PermissionContext.BOT_OWNER);
     }
-
-    private static final Emoji plus = EmojiManager.getForAlias("heavy_plus_sign");
-    private static final Emoji minus = EmojiManager.getForAlias("heavy_minus_sign");
 
     @Override
     public void invoke(MessageParser msgp) throws CommandException {
