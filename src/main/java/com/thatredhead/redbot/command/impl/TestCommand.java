073d79ecb4c80f395f9231c8eb6b7db02e938064
James Gallicchio <jamesgallicchio@gmail.com>
2017-06-23 00:38:52 -0400

    Make test command work!!! :<

--- src/main/java/com/thatredhead/redbot/command/impl/TestCommand.java
+++ src/main/java/com/thatredhead/redbot/command/impl/TestCommand.java
@@ -13,26 +13,26 @@ import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestCommand extends Command {
-    public static final Emoji LOWER = EmojiManager.getForAlias("heavy_minus_sign");
-    public static final Emoji RAISE = EmojiManager.getForAlias("heavy_plus_sign");
+    public static final Emoji LOWER = EmojiManager.getByUnicode("\u2796");
+    public static final Emoji RAISE = EmojiManager.getByUnicode("\u2795");
 
     private long lastMessage;
 
     public TestCommand() {
-        super("test", "TESTY COMMANDS", PermissionContext.BOT_OWNER);
+        super("test", "TESTY COMMAND", PermissionContext.BOT_OWNER);
     }
 
     @Override
     public void invoke(MessageParser msgp) throws CommandException {
         Utilities4D4J.removeReactionUI(lastMessage);
         AtomicInteger num = new AtomicInteger();
-        Utilities4D4J.sendReactionUI(num.toString(), msgp.getChannel(), (m, u, e) -> {
-            if (LOWER.equals(e)) {
+        lastMessage = Utilities4D4J.sendReactionUI(num.toString(), msgp.getChannel(), (m, u, e) -> {
+            if (RAISE.equals(e)) {
                 num.incrementAndGet();
             } else {
                 num.decrementAndGet();
             }
             Utilities4D4J.edit(m, num.toString());
-        });
+        }, LOWER, RAISE).getLongID();
     }
 }
