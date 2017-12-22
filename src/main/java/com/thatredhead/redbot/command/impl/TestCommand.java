d20fc935ffb639908074f421b9df4179304f9c35
jgallicchio <jamesgallicchio@gmail.com>
2017-12-22 08:02:14 -0500

    Merge conflicts

--- src/main/java/com/thatredhead/redbot/command/impl/TestCommand.java
+++ src/main/java/com/thatredhead/redbot/command/impl/TestCommand.java
@@ -1,10 +1,6 @@
 package com.thatredhead.redbot.command.impl;
 
-import com.rometools.rome.feed.synd.SyndEntry;
-import com.rometools.rome.feed.synd.SyndFeed;
-import com.rometools.rome.io.FeedException;
-import com.rometools.rome.io.SyndFeedInput;
-import com.rometools.rome.io.XmlReader;
+import com.thatredhead.redbot.RedBot;
 import com.thatredhead.redbot.command.Command;
 import com.thatredhead.redbot.command.CommandException;
 import com.thatredhead.redbot.helpers4d4j.MessageParser;
@@ -15,9 +11,6 @@ import com.vdurmont.emoji.EmojiManager;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.RequestBuffer;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TestCommand extends Command {
@@ -28,20 +21,17 @@ public class TestCommand extends Command {
 
     @Override
     public void invoke(MessageParser msgp) throws CommandException {
-        String subUrl = msgp.getContentAfter(1);
+        if (msgp.getArgCount() < 2) msgp.reply("Specify count!");
+        else {
+            IChannel c = msgp.getChannel();
+            String mention = msgp.getAuthor().mention();
+            int count = Integer.parseInt(msgp.getArg(1));
 
-        try {
-            URL url = new URL(subUrl);
-
-            SyndFeed feed = new SyndFeedInput().build(new XmlReader(url));
-
-            for (int i = 5; i < feed.getEntries().size(); i++) {
-                List<SyndEntry> entries = feed.getEntries().subList(i, i+1);
-
-                msgp.getChannel().sendMessage(new SubscriberCommands.FeedEmbed(feed, entries).toEmbed());
+            for (int i = 0; i < count; i++) {
+                RequestBuffer.request(() -> {
+                    c.sendMessage(mention);
+                }).get();
             }
-        } catch (IOException | FeedException e) {
-            e.printStackTrace();
         }
     }
 }
