3d43f902ca02bb0391d3cfbb852464210c5fd286
James Gallicchio <jamesgallicchio@gmail.com>
2017-12-31 12:52:44 -0500

    Fix up compiler errors

--- src/main/java/com/thatredhead/redbot/command/impl/TriviaCommand.java
+++ src/main/java/com/thatredhead/redbot/command/impl/TriviaCommand.java
@@ -4,13 +4,13 @@ import com.thatredhead.redbot.command.Command;
 import com.thatredhead.redbot.command.CommandException;
 import com.thatredhead.redbot.helpers4d4j.MessageParser;
 
-public class TriviaCommand extends Command {
+public class TriviaCommand {
 
     public TriviaCommand() {
         super();
     }
 
-    @Override
+    
     public void invoke(MessageParser msgp) throws CommandException {
 
     }
