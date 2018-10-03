0885a5f716e89c9fb9691b1a6d837920dfe5cba0
James Gallicchio <jamesgallicchio@gmail.com>
2018-10-03 12:47:01 -0400

    Add more ? utilities for handling nulls

--- src/main/scala/redbot/utils/OptParams.scala
+++ src/main/scala/redbot/utils/OptParams.scala
@@ -14,4 +14,10 @@ object OptParams {
     def ? : Option[T] = Option(nullable)
     def ?[R](mapper: T => R): Option[R] = Option(nullable) map mapper
   }
+
+  def ?[T1 <: AnyRef, T2 <: AnyRef](t1: T1, t2: T2): Option[(T1, T2)] =
+    if (t1 == null || t2 == null) None else Some((t1, t2))
+
+  def ?[T1 <: AnyRef, T2 <: AnyRef, T3 <: AnyRef](t1: T1, t2: T2, t3: T3): Option[(T1, T2, T3)] =
+    if (t1 == null || t2 == null || t3 == null) None else Some((t1, t2, t3))
 }
