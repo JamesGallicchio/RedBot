202b9ecb2ba30bed393d7c90c84bf40473038410
James Gallicchio <jamesgallicchio@gmail.com>
2019-05-28 00:12:56 -0400

    Add URL verification and redirect detection to InputUtils

--- src/main/scala/redbot/utils/InputUtils.scala
+++ src/main/scala/redbot/utils/InputUtils.scala
@@ -1,31 +1,47 @@
 package redbot.utils
 
+import scala.util.Try
+import java.net.URL
+
 object InputUtils {
-  def distance(s1: String, s2: String): Int =
+  /**
+    * Levenshtein distance between two strings
+    * @param s1
+    * @param s2
+    * @return the Levenshtein distance
+    */
+  def distance(s1: String, s2: String, ignoreCaps: Boolean = false): Int =
     if (s1.length > s2.length)
-      levDistance(s1, s2)
+      levDistance(s1, s2, ignoreCaps)
     else
-      levDistance(s2, s1)
+      levDistance(s2, s1, ignoreCaps)
 
-  def certainty(s1: String, s2: String): Int =
+  def closeness(s1: String, s2: String, ignoreCaps: Boolean = false): Int =
+    Math.max(s1.length, s2.length) - distance(s1, s2, ignoreCaps)
+
+  @deprecated
+  def certainty(s1: String, s2: String): Double =
     if (s1.length > s2.length)
-      s1.length - levDistance(s1, s2)
+      (s1.length - levDistance(s1, s2)).toDouble/s1.length()
     else
-      s2.length - levDistance(s2, s1)
+      (s2.length - levDistance(s2, s1)).toDouble/s2.length()
 
-  private def levDistance(s1: String, s2: String): Int = {
+  private def levDistance(s1: String, s2: String, ignoreCaps: Boolean = false): Int = {
     var firstRow = (0 to s2.length).toArray
     var secondRow = new Array[Int](firstRow.length)
 
     for (i <- s1.indices) {
 
-      secondRow.update(0,i+1)
+      secondRow(0) = i+1
 
       for (j <- s2.indices) {
         val deletCost = firstRow(j+1)+1
         val insertCost = secondRow(j)+1
-        val subCost = firstRow(j) + (if (s1.charAt(i) == s2.charAt(j)) 0 else 1)
-        secondRow.update(j+1, Math.min(deletCost, Math.min(insertCost, subCost)))
+        val subCost = firstRow(j) +
+          (if (s1.charAt(i) == s2.charAt(j) ||
+            (ignoreCaps && s1.charAt(i).toLower == s2.charAt(j).toLower)) 0 else 1)
+
+        secondRow(j+1) = Math.min(deletCost, Math.min(insertCost, subCost))
       }
 
       val temp = firstRow
@@ -34,5 +50,17 @@ object InputUtils {
     }
 
     firstRow(firstRow.length-1)
+  }
+  
+  def verifyURL(url: String): Try[URL] = Try {
+    val u = new URL(url)
+    val conn = u.openConnection()
+    conn.connect()
+    
+    val is = conn.getInputStream
+    val finalUrl = conn.getURL
+    is.close()
+    
+    finalUrl
   }
 }
