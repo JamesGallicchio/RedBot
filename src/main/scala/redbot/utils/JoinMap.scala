025e085a4ac7f208c1e29d3c5baf845ec19c848c
James Gallicchio <jamesgallicchio@gmail.com>
2019-05-30 22:27:17 -0400

    Fix compile/logic errors in JoinMap and add functionality

--- src/main/scala/redbot/utils/JoinMap.scala
+++ src/main/scala/redbot/utils/JoinMap.scala
@@ -1,24 +1,26 @@
 package redbot.utils
 
-import scala.collections.Map
-import scala.collections.mutable.Map
-import scala.collections.immutable.Set
+import play.api.libs.json.Format
+
+import scala.collection.Map
+import scala.collection.mutable
+import scala.collection.immutable.Set
 
 class JoinMap[L, R] private (private val backingL: mutable.Map[L, Set[R]],
                              private val backingR: mutable.Map[R, Set[L]]) {
-  def getL(key: L): Option[Set[R]] = backingL(key)
-  def getR(key: R): Option[Set[L]] = backingR(key)
+  def getL(key: L): Option[Set[R]] = backingL.get(key)
+  def getR(key: R): Option[Set[L]] = backingR.get(key)
   
   def join(left: L, right: R): Unit = {
-    backingL(left)  = backingL.getOrElse(left, new Set) + right
-    backingR(right) = backingR.getOrElse(right, new Set) + left
+    backingL(left)  = backingL.getOrElse(left,  Set.empty) + right
+    backingR(right) = backingR.getOrElse(right, Set.empty) + left
   }
   
   def unjoin(left: L, right: R): Boolean =
     (for {
-      rSet <- backingL(left)
+      rSet <- getL(left)
       if rSet contains right
-      lSet <- backingR(right)
+      lSet <- getR(right)
       if lSet contains left
     } yield {
       backingL(left) = rSet - right
@@ -26,21 +28,24 @@ class JoinMap[L, R] private (private val backingL: mutable.Map[L, Set[R]],
     }).isDefined
   
   def isJoined(left: L, right: R): Boolean =
-    backingL(left).filter(_ contains right).isDefined
+    getL(left).exists(_ contains right)
   
-  def flip(): JoinMap[R, L] = new JoinMap(backingR, backingL)
+  def flip(): JoinMap[R, L] = new JoinMap(backingR.clone(), backingL.clone())
   
-  def clone(): JoinMap[L, R] = new JoinMap(backingL.clone(), backingR.clone())
+  override def clone(): JoinMap[L, R] = new JoinMap(backingL.clone(), backingR.clone())
   
-  def keyedLeft(): Map[L, Set[R]] = backingL.clone()
-  def keyedRight(): Map[R, Set[L]] = backingR.clone()
+  def keyedLeft: Map[L, Set[R]] = backingL
+  def keyedRight: Map[R, Set[L]] = backingR
+
+  def keysL: Iterable[L] = backingL.keys
+  def keysR: Iterable[R] = backingR.keys
 }
 
 object JoinMap {
-  def apply[L, R](): JoinMap[L, R] = new JoinMap(new mutable.Map, new mutable.Map)
+  def empty[L, R]: JoinMap[L, R] = new JoinMap(mutable.Map.empty, mutable.Map.empty)
   
-  def apply[L, R](map: Map[L, R]): JoinMap[L, R] = {
-    val j = JoinMap()
+  def apply[L, R](map: Map[L, Set[R]]): JoinMap[L, R] = {
+    val j = JoinMap.empty[L, R]
     for {
       (l, rSet) <- map
       r <- rSet
@@ -49,20 +54,24 @@ object JoinMap {
   }
   
   implicit def LeftKeyedFormat[A, B](implicit keyFormat: Format[A],
-                                              valuesFormat: Format[Set[B]]): Format[JoinMap[A, B]] = {
-    import play.api.libs.json.Reads._
-    import play.api.libs.json.Writes._
+                                              valuesFormat: Format[B]): Format[JoinMap[A, B]] = {
+    import play.api.libs.json.Reads
+    import play.api.libs.json.Writes
     
     Format(
-      Reads.ArrayReads[(A, Set[B])].map { case arr => JoinMap(arr.toMap) },
-      Writes.arrayWrites[(A, Set[B])].contramap[JoinMap[A, B]](_.keyedLeft())
+      Reads.ArrayReads[(A, Set[B])].map { arr => JoinMap(arr.toMap) },
+      (o: JoinMap[A, B]) => Writes.arrayWrites[(A, Set[B])].writes(o.keyedLeft.toArray)
     )
   }
-  
+
   implicit def RightKeyedFormat[A, B](implicit keyFormat: Format[B],
-                                               valuesFormat: Format[Set[A]])): Format[JoinMap[A, B]] =
+                                     valuesFormat: Format[A]): Format[JoinMap[A, B]] = {
+    import play.api.libs.json.Reads
+    import play.api.libs.json.Writes
+
     Format(
-      LeftKeyedFormat[A, B].map(_.flip()),
-      LeftKeyedFormat[A, B].contramap(_.flip())
+      Reads.ArrayReads[(B, Set[A])].map { arr => JoinMap(arr.toMap).flip() },
+      (o: JoinMap[A, B]) => Writes.arrayWrites[(B, Set[A])].writes(o.keyedRight.toArray)
     )
+  }
 }
